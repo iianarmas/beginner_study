7fb5591865390e21396d8c27498121b340912d47
iian armas <armas.cav@gmail.com>
2021-08-23 15:26:25 +0800

    Create mdchip.py

---
+++ mdchip.py
@@ -0,0 +1,26 @@
+MDBoxLayout:
+    spacing: "20dp"
+    padding: "20dp"
+    MDChooseChip
+        MDChip:
+            text: "Facebook"
+            text_color: 1, 1, 1, 1
+            icon_color: 1, 1, 1, 1
+            icon: "facebook"
+            check: True
+            selected_chip_color: .21176470535294, .098039627451, 1, 1
+            radius: [dp(8)]
+        MDChip:
+            text: "Google"
+            text_color: 1, 1, 1, 1
+            icon_color: 1, 1, 1, 1
+            icon: "google"
+            check: True
+            selected_chip_color: .21176470535294, .098039627451, 1, 1
+        MDChip:
+            text: "YouTube"
+            text_color: 1, 1, 1, 1
+            icon_color: 1, 1, 1, 1
+            icon: "youtube"
+            check: True
+            selected_chip_color: .21176470535294, .098039627451, 1, 1
