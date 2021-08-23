e3fd18ca0f0fecae31c4a51115ee59ce0c52e1b6
iian armas <armas.cav@gmail.com>
2021-08-23 16:23:28 +0800

    Create pickers.py

---
+++ pickers.py
@@ -0,0 +1,12 @@
+MDBoxLayout:
+    size_hint: 0.5, 0.5
+    pos_hint: {"center_x": 0.5, "center_y": 0.5}
+    MDRaisedButton:
+        text: "Date Picker"
+        on_release: app.show_date()
+    MDRaisedButton:
+        text: "Time Picker"
+        on_release: app.show_time()
+    MDRaisedButton:
+        text: "Color Picker"
+        on_release: app.show_theme()
