23d21491250fb7552ec3d4dc1a2e2d8319dc88e4
iian armas <armas.cav@gmail.com>
2021-08-23 22:23:18 +0800

    Update card_expansion_panel.py
    fixed the way panel opens and closes

--- card_expansion_panel.py
+++ card_expansion_panel.py
@@ -1,26 +1,32 @@
+from kivy.animation import Animation
 from kivymd.app import MDApp
 from kivymd.uix.boxlayout import MDBoxLayout
-from kivymd.uix.expansionpanel import MDExpansionPanel, MDExpansionPanelThreeLine, MDExpansionPanelLabel, MDExpansionPanelOneLine
+from kivymd.uix.expansionpanel import MDExpansionPanel, MDExpansionPanelThreeLine, MDExpansionPanelLabel, \
+    MDExpansionPanelOneLine
 from kivy.lang import Builder
 
 smp = """
 MDGridLayout:
-    
     cols: 2
     MDRelativeLayout:
         MDCard:
-            id: box
+            id: card
             orientation: "vertical"
             pos_hint: {"center_x": 0.55, "center_y": 0.5}
-            size_hint: 0.8, 0.75
-            #adaptive_height: True
+            size_hint: 0.8, None
+            height: self.minimum_height
             padding: "10dp"
             spacing: "10dp"
-            Image:
-                source: "nezuko.png"
-                size_hint: 1, 0.8
-                allow_stretch: True
-                keep_ratio: False
+            MDBoxLayout:
+                id: box
+                size_hint_y: None
+                height: self.minimum_height
+                Image:
+                    source: "nezuko.png"
+                    size_hint: 1, None
+                    height: "300dp"
+                    #allow_stretch: True
+                    #keep_ratio: False
 
 
 
@@ -44,8 +50,10 @@ MDGridLayout:
 
 
 <Box>:
-    adaptive_height: True
+    #adaptive_height: True
     orientation: "vertical"
+    size_hint_y: None
+    height: "150dp"        
     MDLabel:
         text: "Sample Layout"
         font_style: "Subtitle1"
@@ -57,29 +65,41 @@ MDGridLayout:
         MDRectangleFlatButton:
             text: "Cancel"
             font_style: "Button"
-            
-            
-        
+    
 """
 
 
 class Box(MDBoxLayout):
     """pass"""
 
+
 class DemoApp(MDApp):
     def build(self):
         return Builder.load_string(smp)
 
     def on_start(self):
-        self.root.ids.box.add_widget(
-            MDExpansionPanel(
-                icon='language-python',
-                content=Box(),
-                panel_cls=MDExpansionPanelOneLine(
-                    text='Nezuko'
-                )
+        content = Box()
+        panel = MDExpansionPanel(
+            on_open=self.panel_open,
+            on_close=self.panel_close,
+            icon='language-python',
+            content=content,
+            panel_cls=MDExpansionPanelOneLine(
+                text='Nezuko'
             )
+
         )
+        self.root.ids.card.add_widget(panel)
+
+    def panel_open(self, *args):
+        Animation(
+            height=(self.root.ids.box.height - self.root.ids.content.height) + self.theme_cls.standard_increment
+                   * 2, d=0.2).start(self.root.ids.box)
+
+    def panel_close(self, *args):
+        Animation(
+            height=(self.root.ids.box.height - self.root.ids.content.height) +
+                   self.theme_cls.standard_increment * 2, d=0.2).start(self.root.ids.box)
 
 
 if __name__ == "__main__":
