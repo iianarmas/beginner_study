5a302bdafe707408e7ca6350c4790d947df43b6c
iian armas <armas.cav@gmail.com>
2021-09-30 16:58:17 +0800

    Update network.py

--- network.py
+++ network.py
@@ -62,3 +62,50 @@ for i, element in enumerate(head):
 
 for key, value in element_dict.items():
     print(f'{key}: {value}')
+
+    # ==========================
+## Customizing Request
+
+from urllib.request import urlopen, Request
+
+
+"""req = Request('http://www.debian.org')"""
+
+# customizing request
+"""req.add_header('Accept-Language', 'sv')
+
+response = urlopen(req)
+
+
+print(response.readlines()[:5])
+print()
+print(req.header_items())
+print()"""
+
+# shortcut for adding headers
+
+header = {'Accept-Language': 'sv'}
+
+req = Request('http://www.debian.org', headers=header)
+
+response = urlopen(req)
+
+
+print(response.readlines()[:5])
+print()
+print(req.header_items())
+print()
+
+# getting headers ----------
+head = response.getheaders()
+
+# by default, getheaders() will return a list of tuples,
+# creating a dictionary is one option for nicer printing of info (I think)
+
+element_dict = {}
+
+for i, element in enumerate(head):
+    element_dict[element[0]] = element[1]
+
+for key, value in element_dict.items():
+    print(f'{key}: {value}')
